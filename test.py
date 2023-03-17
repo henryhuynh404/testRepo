2746dc14a05d074f6575408b5ca47556c219a506
Henry Huynh <henry.huynh@sjtechcorp.com>
2023-03-17 14:16:23 -0400

    Change message

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 def application(environ, start_response):
     status = '200 OK'
-    output = b'Hello Howtoforge!\n'
+    output = b'Hello From Python!\n'
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(output)))]
     start_response(status, response_headers)

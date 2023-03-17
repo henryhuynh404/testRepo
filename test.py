ea20d21d5b50503b76ae33911358b6d1aa72242d
Henry Huynh <henry.huynh@sjtechcorp.com>
2023-03-17 14:22:25 -0400

    Streamlit UI

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hello From Python!\n'
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output] 
+import streamlit as st
+
+col1, col2 = st.columns(2)
+with col1: 
+    st.image("sj.jpeg")
+    st.write("SJ Dashboard")

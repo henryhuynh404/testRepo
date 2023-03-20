842da18b6fee0591eb5756390176af4f5167ba19
Henry Huynh <henry.huynh@sjtechcorp.com>
2023-03-20 14:20:01 -0400

    Deleted test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def application(environ, start_response):
-    status = '200 OK'
-    output = b'Hello From Python!\n'
-    response_headers = [('Content-type', 'text/plain'),
-                        ('Content-Length', str(len(output)))]
-    start_response(status, response_headers)
-    return [output] 
